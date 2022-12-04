5c4c9b7a7c5881ba5d96f31c400f0f48f2a16f7b
szdanika <danikaszab@gmail.com>
2022-12-04 16:48:16 +0200

    ini

---
+++ WebAutoThingi/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
